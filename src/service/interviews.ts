69c04005002479ebcd6b07020cd03eced2847b3e
sindhu12983 <sindhuale12@gmail.com>
2026-06-12 08:58:18 +0530

    Fix import statement for pool in interviews.ts

--- src/service/interviews.ts
+++ src/service/interviews.ts
@@ -1,5 +1,5 @@
 import { Request,Response } from "express";
-import pool from "../db";
+import { pool } from '../db'
 import { PoolClient } from "pg";
 import { cInterview,uInterview,interview } from "../models";
 export const createInterview=async (req:Request,res:Response)=>{
